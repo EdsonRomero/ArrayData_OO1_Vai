6d5bf92a5717724b8fa7a38ba855b1b57436e2e6
Edson Romero <edson.romero@hotmail.com>
2015-11-21 03:19:30 -0200

    Initial commit

---
+++ app/src/main/java/com/example/edson/arraydata_oo1/Evento.java
@@ -0,0 +1,66 @@
+package com.example.edson.arraydata_oo1;
+
+import java.io.Serializable;
+
+
+/**
+ * Created by edson on 18/11/15.
+ */
+public class Evento implements Serializable {
+    private String nomeEvento;
+    private String dia;
+    private String mes;
+    private String ano;
+
+
+
+   public Evento(String nomeEvento,int dia, int mes, int ano){
+       setNomeEvento(nomeEvento);
+
+       setAno(String.valueOf(ano));
+       setDia(String.valueOf(dia));
+       setMes(String.valueOf(mes));
+
+
+
+
+
+   }
+
+
+    public String getNomeEvento() {
+        return nomeEvento;
+    }
+
+    public void setNomeEvento(String nomeEvento) {
+        this.nomeEvento = nomeEvento;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+
+
+
+}
